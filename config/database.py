f27590a441e37beae37e41a177922519e2e157f8
Samuel-RM <samiriverame07@gmail.com>
2023-02-27 22:40:11 -0600

    Agregando models, configurando .gitignore y requiremnts

--- config/database.py
+++ config/database.py
@@ -4,7 +4,10 @@ from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 sqlite_file_name = "database.sqlite"
+# Asiganamos un nombre a la base
+
 base_dir = os.path.dirname(os.path.realpath(__file__))
+# 
 
 database_url = f"sqlite:///{os.path.join(base_dir, sqlite_file_name)}"
 
